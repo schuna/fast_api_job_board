d986141ab48895115963bd322a83ecdaff1dd15f
schuna <jiyong1972@gmail.com>
2023-04-23 21:09:09 +0900

    job routes

--- backend/apis/version1/route_jobs.py
+++ backend/apis/version1/route_jobs.py
@@ -1,7 +1,9 @@
+from typing import List
+
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 
-from db.repository.jobs import create_new_job, retrieve_job
+from db.repository.jobs import create_new_job, retrieve_job, list_jobs, update_job_by_id, delete_job_by_id
 from db.session import get_db
 from schemas.jobs import JobCreate, ShowJob
 
@@ -22,3 +24,29 @@ def retrieve_job_by_id(id: int, db: Session = Depends(get_db)):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"Job with id {id} does not exist")
     return job
+
+
+@router.get("/all", response_model=List[ShowJob])
+def retrieve_all_jobs(db: Session = Depends(get_db)):
+    jobs = list_jobs(db=db)
+    return jobs
+
+
+@router.put("/update/{id}")
+def update_job(id: int, job: JobCreate, db: Session = Depends(get_db)):
+    owner_id = 1
+    message = update_job_by_id(id=id, job=job, db=db, owner_id=owner_id)
+    if not message:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Job with id {id} does not exist")
+    return {"detail": "Successfully updated data."}
+
+
+@router.delete("/delete/{id}")
+def delete_job(id: int, db: Session = Depends(get_db)):
+    owner_id = 1
+    message = delete_job_by_id(id=id, db=db, owner_id=owner_id)
+    if not message:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Job with id {id} does not exist")
+    return {"detail": "Successfully deleted data."}
